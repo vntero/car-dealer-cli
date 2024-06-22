04300632556e421a332a2e2374040cd1b39f067a
vntero <shin.antero@gmail.com>
2024-06-22 13:08:03 +0200

    adds comments with types of prompt to the app dot js file

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
 import inquirer from 'inquirer'
 import { Command } from 'commander'
 
-console.log('Hello World!')
+// send welcome message to user
+console.log(
+  'Hey there, stranger! I am here to help you find out more about our fleet. Write <help> to know all the commands available.'
+)
+
+// read the csv file
+
+// execute commands
+// ----- help
+
+// ----- number-of-cars-by-brand
+
+// ----- list-of-cars-by-brand
+
+// ----- list-of-cars-by-mileage-range
+
+// ----- total-value-by-dealership
+// ---------- make sure that user knows which dealerships are there
