a36e6759a6e794668abcd777c7447a6baf2148dc
vntero <shin.antero@gmail.com>
2024-06-22 12:50:52 +0200

    lists possible usecases

---
+++ src/usecases.js
@@ -0,0 +1,7 @@
+// get the number of cars by brand (parameter: brand)
+
+// get the number of cars and the list of cars by brand (parameter: brand)
+
+// get the number of cars and the list by mileage range (parameter: maxMileage, minMileage)
+
+// get the total value of cars that exist in a given dealership
