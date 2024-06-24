a9fd1e4387a329e5717dfc7b3d06024949cfc628
vntero <shin.antero@gmail.com>
2024-06-24 14:37:00 +0200

    fixes prettier error

--- src/app.js
+++ src/app.js
@@ -10,13 +10,14 @@ const cars = []
 
 // send welcome message to user
 console.log(
-  'Hey there, stranger! I am here to help you find out more about our fleet. Write <help> to know all the commands available.'
+  'Hey there, stranger! I am here to help you find out more about our fleet. Write <help> to know all the commands available.',
 )
 
 // read the csv file
 const readData = async (csvFile) => {
   return new Promise((resolve, reject) => {
-    const stream = fs.createReadStream(csvFile)
+    const stream = fs
+      .createReadStream(csvFile)
       .pipe(csv())
       .on('data', (data) => cars.push(data))
       .on('end', () => resolve(cars))
@@ -40,20 +41,11 @@ query
   .action()
 
 // ----- list-of-cars-by-brand
-query
-  .command()
-  .description()
-  .action()
+query.command().description().action()
 
 // ----- list-of-cars-by-mileage-range
-query
-  .command()
-  .description()
-  .action()
+query.command().description().action()
 
 // ----- total-value-by-dealership
 // ---------- make sure that user knows which dealerships are there
-query
-  .command()
-  .description()
-  .action()
+query.command().description().action()
