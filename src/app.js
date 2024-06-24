fbdbabf6fa8d41cce86d7dc782033eadb629b123
vntero <shin.antero@gmail.com>
2024-06-24 16:05:57 +0200

    app works but always returns 0

--- src/app.js
+++ src/app.js
@@ -2,22 +2,65 @@ import { Command } from 'commander'
 import path from 'path'
 import csv from 'csv-parser'
 import fs from 'fs'
-import * as csvFile from './data/cars.csv'
 
 const query = new Command()
 
 const cars = []
 
-// send welcome message to user
-console.log(
-  'Hey there, stranger! I am here to help you find out more about our fleet. Write <help> to know all the commands available.',
-)
+// // send welcome message to user
+// console.log(
+//   'Hey there, stranger! I am here to help you find out more about our fleet. Write <help> to know all the commands available.',
+// )
+
+// Usecases
+// ----- get the number of cars by brand (parameter: brand)
+const getCarsByBrand = (brand) => {
+  const filteredCarsByBrand = cars.filter((car) => car.brand === brand)
+  console.log(
+    `For the brand you entered, the dealership has a total of ${filteredCarsByBrand.length} cars`,
+  )
+}
+
+// ----- get the number of cars and the list of cars by brand (parameter: brand)
+const listCarsByBrand = (brand) => {
+  const filteredCarsByBrand = cars.filter((car) => car.brand === brand)
+  console.log(
+    `For the brand you entered, the dealership has a total of ${filteredCarsByBrand.length} cars`,
+  )
+  console.log(
+    `Here's a list of the ${brand} cars we have: `,
+    filteredCarsByBrand,
+  )
+}
+
+// ------ get the number of cars and the list by mileage range (parameter: maxMileage, minMileage)
+const listCarsByMileageRange = (minMileage, maxMileage) => {
+  const filteredCars = cars.filter(
+    (car) => car.mileage >= minMileage && car.mileage <= maxMileage,
+  )
+  console.log(
+    `Number of cars with mileage between ${minMileage} and ${maxMileage}: ${filteredCars.length}`,
+  )
+  console.log(`Here's a list of the ${brand} cars we have: `)
+}
+
+// ------ get the total value of cars that exist in a given dealership
+const getTotalValueByDealership = (dealership) => {
+  const filteredCars = cars.filter((car) => car.dealership === dealership)
+  const totalValue = filteredCars.reduce(
+    (sum, car) => sum + parseFloat(car.price),
+    0,
+  )
+  console.log(
+    `Total value of cars in the "${dealership}" dealership is: €${totalValue}`,
+  )
+}
 
 // read the csv file
-const readData = async (csvFile) => {
+const readData = async (filePath) => {
   return new Promise((resolve, reject) => {
     const stream = fs
-      .createReadStream(csvFile)
+      .createReadStream(filePath)
       .pipe(csv())
       .on('data', (data) => cars.push(data))
       .on('end', () => resolve(cars))
@@ -37,15 +80,49 @@ query
 // ----- number-of-cars-by-brand
 query
   .command('get-cars-by-brand <brand>')
-  .description('Gets the number of cars by brand')
-  .action()
+  .description('Gets the number of cars by brand (parameter: brand)')
+  .action((brand) => {
+    getCarsByBrand(brand)
+  })
 
 // ----- list-of-cars-by-brand
-query.command().description().action()
+query
+  .command('list-cars-by-brand <brand>')
+  .description(
+    'Get the number of cars and the list of cars by brand (parameter: brand)',
+  )
+  .action((brand) => {
+    listCarsByBrand(brand)
+  })
 
 // ----- list-of-cars-by-mileage-range
-query.command().description().action()
+query
+  .command('list-cars-by-mileage-range <minMileage> <maxMileage>')
+  .description(
+    'Get the number of cars and the list by mileage range (parameter: maxMileage, minMileage)',
+  )
+  .action((minMileage, maxMileage) => {
+    listCarsByMileageRange(Number(minMileage), Number(maxMileage))
+  })
 
 // ----- total-value-by-dealership
 // ---------- make sure that user knows which dealerships are there
-query.command().description().action()
+query
+  .command('total-value-by-dealership <dealership>')
+  .description('Get the total value of cars that exist in a given dealership')
+  .action((dealership) => {
+    getTotalValueByDealership(dealership)
+  })
+
+const temp = './data/cars.csv'
+;(async () => {
+  const filePath = path.resolve(
+    '/Users/vntero/Desktop/github/car-dealer-cli/src/data/cars.csv',
+  )
+  try {
+    await readData(filePath)
+    query.parse(process.argv)
+  } catch (err) {
+    console.error('Failed to load CSV file', err)
+  }
+})()
