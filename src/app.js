48774960d1b9b490b10e5f0363371ffac2699ddf
vntero <shin.antero@gmail.com>
2024-06-24 14:33:45 +0200

    adds query to get all cars by brand

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import { Command } from 'commander'
+import path from 'path'
+import csv from 'csv-parser'
+import fs from 'fs'
+import * as csvFile from './data/cars.csv'
 
 const query = new Command()
 
@@ -10,15 +14,46 @@ console.log(
 )
 
 // read the csv file
+const readData = async (csvFile) => {
+  return new Promise((resolve, reject) => {
+    const stream = fs.createReadStream(csvFile)
+      .pipe(csv())
+      .on('data', (data) => cars.push(data))
+      .on('end', () => resolve(cars))
+      .on('error', (err) => reject(err))
+  })
+}
 
 // execute commands
 // ----- help
+query
+  .command('help')
+  .description('Displays the available functions and what they do')
+  .action(() => {
+    query.outputHelp()
+  })
 
 // ----- number-of-cars-by-brand
+query
+  .command('get-cars-by-brand <brand>')
+  .description('Gets the number of cars by brand')
+  .action()
 
 // ----- list-of-cars-by-brand
+query
+  .command()
+  .description()
+  .action()
 
 // ----- list-of-cars-by-mileage-range
+query
+  .command()
+  .description()
+  .action()
 
 // ----- total-value-by-dealership
 // ---------- make sure that user knows which dealerships are there
+query
+  .command()
+  .description()
+  .action()
