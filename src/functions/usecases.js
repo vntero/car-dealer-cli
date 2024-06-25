45cc5019216971bfda8cf562daf723245d533e57
vntero <shin.antero@gmail.com>
2024-06-25 15:22:55 +0200

    main app finished, moving on to testing

--- src/functions/usecases.js
+++ src/functions/usecases.js
@@ -1,5 +1,5 @@
 import { cars } from '../data/loadData.js'
-import { numberValidation, stringValidation } from './validation.js'
+import { numberValidation, stringValidation } from './validations.js'
 
 const processCarsByBrand = (brand, actionType) => {
   if (!stringValidation(brand)) {
@@ -12,16 +12,16 @@ const processCarsByBrand = (brand, actionType) => {
 
   if (filteredCarsByBrand.length === 0) {
     console.log(
-      `Currently there are no ${brand} cars to be ${actionType}. Here's a list of available brands: ${uniqueBrands}.`,
+      `Currently there are no "${brand}" cars to be ${actionType}. Here's a list of available brands: ${uniqueBrands}.`,
     )
   } else {
     if (actionType === 'counted') {
       console.log(
-        `There is a total of ${filteredCarsByBrand.length} ${brand} cars.`,
+        `There is a total of ${filteredCarsByBrand.length} "${brand}" cars.`,
       )
     } else if (actionType === 'listed') {
       console.log(
-        `There is a total of ${filteredCarsByBrand.length} ${brand} cars. Find them listed below: ${JSON.stringify(filteredCarsByBrand, null, 2)}`,
+        `There is a total of ${filteredCarsByBrand.length} "${brand}" cars. Find them listed below: ${JSON.stringify(filteredCarsByBrand, null, 2)}`,
       )
     }
   }
@@ -54,15 +54,20 @@ export const listCarsByMileageRange = (minMileage, maxMileage) => {
 }
 
 export const getTotalValueByDealership = (dealership) => {
-  const filteredCars = cars.filter((car) => car.Dealership === dealership)
+  if (!stringValidation(dealership)) {
+    return console.error('Input must be a non-empty string.')
+  }
+  const filteredCarsByDealership = cars.filter(
+    (car) => car.Dealership === dealership,
+  )
   const uniqueDealerships = [...new Set(cars.map((car) => car.Dealership))]
-  const totalValue = filteredCars.reduce(
+  const totalValue = filteredCarsByDealership.reduce(
     (sum, car) => sum + parseFloat(car.Price),
     0,
   )
-  filteredCars.length === 0
+  filteredCarsByDealership.length === 0
     ? console.log(
-        `Currently there is no "${dealership}" dealership in our records. Here's a list of available dealerships: ${uniqueDealerships}. Also, make sure you add quotation marks for searching dealerships with more than one word.`,
+        `Currently there is no "${dealership}" dealership in our records. Here's a list of available dealerships: ${uniqueDealerships}. Also, make sure to add quotation marks when searching dealerships with more than one word.`,
       )
     : console.log(
         `The total value of cars in the "${dealership}" dealership is: €${totalValue}`,
